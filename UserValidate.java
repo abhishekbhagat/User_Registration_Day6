53c3c222368fd487e0693653aeb71e3f154b7e36
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-06 07:40:18 +0530

    [abhishek] add uc2 case: to check last name is valid or not

--- UserValidate.java
+++ UserValidate.java
@@ -4,9 +4,14 @@ import java.util.regex.Pattern;
 
 public class UserValidate {
 	private static final String FIRST_NAME_PATTERN = "[A-Z]{1}[a-zA-Z]{2,}";
-	
+	private static final String LAST_NAME_PATTERN = "[A-Z]{1}[a-zA-Z]{2,}";
+
 	public boolean validFirstName(String firstName) {
 		Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
 		return pattern.matcher(firstName).matches();
+	}
+
+	public boolean validLastName(String lastName) {
+		return Pattern.matches(LAST_NAME_PATTERN, lastName);
 	}
 }
--- UserValidateTest.java
+++ UserValidateTest.java
@@ -11,11 +11,25 @@ public class UserValidateTest {
 		boolean result = uservalidate.validFirstName("Abhisehk");
 		Assert.assertEquals(true, result);
 	}
-	
+
 	@Test
 	public void givenFirstName_WhenProper_ShouldReturnFalse() {
 		UserValidate uservalidate = new UserValidate();
 		boolean result = uservalidate.validFirstName("abhisehk");
+		Assert.assertEquals(false, result);
+	}
+
+	@Test
+	public void givenLast_WhenProper_ShouldReturnTrue() {
+		UserValidate uservalidate = new UserValidate();
+		boolean result = uservalidate.validLastName("Abhisehk");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLast_WhenProper_ShouldReturnFalse() {
+		UserValidate uservalidate = new UserValidate();
+		boolean result = uservalidate.validLastName("Abhisehk");
 		Assert.assertEquals(false, result);
 	}
 }
